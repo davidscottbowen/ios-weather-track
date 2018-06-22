1ed8de1875bb3816b96c18966f46d020437b7a6f
Scott <david.bowen@interapthq.com>
2018-06-21 21:39:52 -0400

    added Alamofire call to darkskies

---
+++ weather-track/Components/Constants.swift
@@ -0,0 +1,178 @@
+//
+//  Constants.swift
+//  weather-track
+//
+//  Created by David Bowen on 6/21/18.
+//  Copyright © 2018 David Bowen. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    
+    static let apiKey = "ApiKey"
+    static let ApiKey = "cpsp3g6kz9da"
+    
+    
+    struct API {
+        static let CurrentlyKey = "currently"
+        static let TemperatureKey = "temperature"
+        static let DisplayNameKey = "displayName"
+        static let ScheduleKey = "Schedule"
+        static let ARIKey = "ARI"
+        static let GameIdKey = "gameId"
+        static let GameWeekKey = "gameWeek"
+        static let GameDateKey = "gameDate"
+        static let AwayTeamKey = "awayTeam"
+        static let HomeTeamKey = "homeTeam"
+        static let GameTimeKey = "gameTimeET"
+        static let TvStationKey = "tvStation"
+        static let InjuriesKey = "Injuries.ARI:"
+        static let PlayerNameKey = "playerName"
+        static let Team = "team"
+        static let Position = "position"
+        static let PlayersKey = "Players"
+        static let RankingsKey = "Rankings"
+        static let TeamCodeKey = "team"
+        static let NameKey = "name"
+        
+        
+        
+        //QuarterBack
+        static let PassAtt = "passAtt"
+        static let PassCmp = "passCmp"
+        static let PassYds = "passYds"
+        static let PassTD = "passTD"
+        
+        //WideReceiver
+        static let receptions = "receptions"
+        static let recYds = "recYds"
+        static let recTD = "recTD"
+        
+        //RunningBack
+        static let rushAtt = "rushAtt"
+        static let rushYds = "rushYds"
+        //   static let receptions = "receptions"
+        //   static let recYds = "recYds"
+        //   static let recTD = "recTD"
+        //TightEnd
+        //   static let receptions = "receptions"
+        //   static let recYds = "recYds"
+        //   static let recTD = "recTD"
+        
+        //Kicker
+        static let fg = "fg"
+        static let fgAtt = "fgAtt"
+        static let xp = "xp"
+        
+        //Defense
+        static let defInt = "defInt"
+        static let defSack = "defSack"
+        static let defPA = "defPA"
+        
+        //NFL Teams
+        static let code = "code"
+        static let fullName = "fullName"
+        static let shortName = "shortName"
+        static let NFLTeams = "NFLTeams"
+        
+        
+        //Players
+        
+        static let PlayerIdKey = "playerId"
+        static let ActiveKey = "active"
+        static let JerseyKey = "jersey"
+        static let LnameKey = "lname"
+        static let FnameKey = "fname"
+        static let DisplayKey = "displayName"
+        static let TeamKey = "team"
+        static let PositionKey = "position"
+        static let HeightKey = "height"
+        static let WeightKey = "weight"
+        static let DOBKey = "dob"
+        static let CollegeKey = "college"
+        
+        //QuarterBack
+        
+        
+        //WideReceiver
+        
+        
+        //RunningBack
+        static let rushTD = "rushTD"
+        
+        //   static let receptions = "receptions"
+        //   static let recYds = "recYds"
+        //   static let recTD = "recTD"
+        //TightEnd
+        //   static let receptions = "receptions"
+        //   static let recYds = "recYds"
+        //   static let recTD = "recTD"
+        
+        
+        
+    }
+    
+    public static let playerId = "playerId"
+    public static let displayName = "displayName"
+    public static let week = "week"
+    public static let position = "position"
+    public static let keychainIdentifier = "FootballKeychain"
+    public static let authTokenExpireDate = "authTokenExpireDate"
+    public static let authToken = "authToken"
+    
+    
+    struct JSON {
+        static let unknownError = "An Unknown Error Has Occurred"
+        static let processingError = "There was an error processing the response"
+    }
+    
+    struct QuarterBack {
+        static let displayName = "displayName"
+        static let passAtt = "passAtt"
+        static let passCmp = "passCmp"
+        static let passYds = "passYds"
+        static let passTD = "passTD"
+    }
+    
+    struct WideReceiver {
+        static let receptions = "receptions"
+        static let recYds = "recYds"
+        static let recTD = "recTD"
+    }
+    
+    struct RunningBack {
+        static let rushAtt = "rushAtt"
+        static let rushYds = "rushYds"
+        static let receptions = "receptions"
+        static let recYds = "recYds"
+        static let recTD = "recTD"
+    }
+    
+    struct TightEnd {
+        static let receptions = "receptions"
+        static let recYds = "recYds"
+        static let recTD = "recTD"
+    }
+    
+    struct Kicker {
+        static let fg = "fg"
+        static let fgAtt = "fgAtt"
+        static let xp = "xp"
+    }
+    
+    struct Defense {
+        static let defInt = "defInt"
+        static let defSack = "defSack"
+        static let defPA = "defPA"
+    }
+    
+    struct NFLTeams {
+        static let code = "code"
+        static let fullName = "fullName"
+        static let shortName = "shortName"
+        static let NFLTeams = "NFLTeams"
+    }
+    
+}
+
---
+++ weather-track/ViewControllers/HomeViewController.swift
@@ -0,0 +1,51 @@
+//
+//  HomeViewController.swift
+//  weather-track
+//
+//  Created by David Bowen on 6/21/18.
+//  Copyright © 2018 David Bowen. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+import AlamofireSwiftyJSON
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Alamofire.request("https://api.darksky.net/forecast/2c0e34dba4c9f289306ed7a074e79353/37.8145,-82.8071").responseJSON { response in
+            switch response.result {
+            case .success(let value) :
+                var json = JSON(value)
+                print(json)
+
+            case .failure(let error) :
+                fatalError(error.localizedDescription)
+                
+            }
+        }
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
