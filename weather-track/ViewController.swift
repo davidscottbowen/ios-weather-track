0e2122cb6ccc56d42ce8c19c69a86485c2048f2a
Scott <david.bowen@interapthq.com>
2018-06-21 20:04:43 -0400

    Initial Commit

---
+++ weather-track/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather-track
+//
+//  Created by David Bowen on 6/21/18.
+//  Copyright © 2018 David Bowen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
